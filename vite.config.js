b3088b2cefecfb9bd92729449730dccb0fd138cb
杨明宇 <yangmingyu@yangmingyudeMacBook-Air.local>
2026-03-28 13:30:36 +0800

    修复 React 实例冲突引起的白屏报错

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/wps-computer-quiz-project/', 
   plugins: [react()],
+  // 增加下面这段配置，强制去重，只使用唯一的一个 React 实例
+  resolve: {
+    dedupe: ['react', 'react-dom']
+  }
 })
